785a9704c01f0d0ee0ae5ca6cb672c45f9653647
Albert <github@albert.sh>
2020-08-13 19:30:45 +0200

    Fix tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -6,6 +6,8 @@ import * as path from "path";
 test("test runs", () => {
   process.env["INPUT_DOMAIN"] = "example.com";
   process.env["INPUT_PUBLISH_DIR"] = "./images";
+  process.env["INPUT_AWS_ACCESS_KEY_ID"] = "mock_id";
+  process.env["INPUT_AWS_SECRET_ACCESS_KEY"] = "mock_secret_key";
   process.env["GITHUB_WORKSPACE"] = ".";
   const ip = path.join(__dirname, "..", "lib", "main.js");
   const options: cp.ExecSyncOptions = {
@@ -14,5 +16,5 @@ test("test runs", () => {
   // No idea how to get past the DNS point
   expect(() => {
     cp.execSync(`node ${ip}`, options).toString();
-  }).toThrow("Found zones: [] for dns:example.com");
+  }).toThrow("The security token included in the request is invalid");
 });
