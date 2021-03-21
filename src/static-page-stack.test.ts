89acc286982fe5a3c329a3d1aab0e6c4d03698c4
Josh <mail@chukwuanu.com>
2021-03-21 23:28:17 +0100

    test 404 redirect routing rule

--- src/static-page-stack.test.ts
+++ src/static-page-stack.test.ts
@@ -11,3 +11,30 @@ test("Empty Stack", () => {
   });
   expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
 });
+
+test("Empty Stack With 404 Redirect Routing Roles", () => {
+  const app = new cdk.App();
+  const stack = new StaticPageStack(app, "MyTestStack", {
+    fullDomain: "sub.example.com",
+    folder: "./images",
+    stackName: "MyTestStack",
+  });
+  expectCDK(stack).to(
+    haveResource("AWS::S3::Bucket", {
+      WebsiteConfiguration: {
+        IndexDocument: "index.html",
+        ErrorDocument: "error.html",
+        RoutingRules: [
+          {
+            RedirectRule: {
+              ReplaceKeyPrefixWith: "/",
+            },
+            RoutingRuleCondition: {
+              HttpErrorCodeReturnedEquals: "404",
+            },
+          },
+        ],
+      },
+    })
+  );
+});
