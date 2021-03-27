3743837bbb273422ffdb2610b897fb306c00345d
Rafael Polonio <rafael.polonio@gmail.com>
2021-03-27 12:27:21 -0300

    feat: tests

--- src/static-page-stack.test.ts
+++ src/static-page-stack.test.ts
@@ -1,13 +1,28 @@
-import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
+import { expect as expectCDK, haveResource, haveResourceLike } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
 import { StaticPageStack } from "./static-page-stack";
 
+const app = new cdk.App();
+const stack = new StaticPageStack(app, "MyTestStack", {
+  fullDomain: "sub.example.com",
+  folder: "./images",
+  stackName: "MyTestStack",
+});
+
 test("Empty Stack", () => {
-  const app = new cdk.App();
-  const stack = new StaticPageStack(app, "MyTestStack", {
-    fullDomain: "sub.example.com",
-    folder: "./images",
-    stackName: "MyTestStack",
-  });
   expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+});
+
+test("404 Redirect", () => {
+  expectCDK(stack).to(haveResourceLike("AWS::CloudFront::Distribution", {
+    DistributionConfig: {
+      CustomErrorResponses: [
+        {
+          ErrorCode: 404,
+          ResponseCode: 200,
+          ResponsePagePath: "/index.html"
+        }
+      ],
+    }
+  }))
 });
